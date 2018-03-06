692fe3babad9bab956c5cae93237d4634fe9ad9a
Hyun Seung Jeong <ahribori@gmail.com>
2018-03-06 11:08:50 +0900

    Setting CORS options

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,32 @@
+require('dotenv').config();
 import express from 'express';
 import cors from 'cors';
+
 const router = express.Router();
 
 import playlist from './playlist';
+
+const { CORS_WHITELIST } = process.env;
+const whitelist = CORS_WHITELIST && CORS_WHITELIST !== '' ? CORS_WHITELIST : [];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(null, false)
+        }
+    }
+};
+
+router.use('*', cors(corsOptions), (req, res, next) => {
+    next();
+});
 
 router.get('/', (req, res) => {
     res.json('OK');
 });
 
-router.use('/api', cors());
 router.use('/api/playlist', playlist);
 
 export default router;
